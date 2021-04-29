374de9f6a5078ac8b3224be4391061fe77b9e7ce
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-29 12:54:53 +0300

    ...spread operator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import Header from './component/Header';
 import './App.css';
 
+let infos = [
+  {
+    firstName: 'Juji',
+    secondName: 'Bonbona',
+    age: 33
+  },
+  {
+    firstName: 'Pavel',
+    secondName: 'Nikolov',
+    age: 32
+  },
+  {
+    firstName: 'Timo',
+    secondName: 'Petrov',
+    age: 24
+  }
+]
+
 
 function App() {
   return (
     <div className="App">
-      <Header name='John' age='66' />
+      <h1>Company Info</h1>
+      { infos.map( info => {
+        return <Header key={ infos.firstName } { ...info } />
+      } ) }
     </div>
   );
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Header = ( props ) => {
-  let { name, age: oldAge } = props
+  let { firstName, secondName, age } = props
   return (
     <div>
-      <h1>his name is { name } and he is { oldAge } years old</h1>
+      <h1>Employ name: { firstName } { secondName } { age } </h1>
     </div>
   )
 }
